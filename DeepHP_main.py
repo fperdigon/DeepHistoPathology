33477d45f288046d7724630904589053d6ca95e8
fperdigon <fperdigon88@gmail.com>
2022-03-02 14:18:13 -0500

    fixing some variables name

--- DeepHP_main.py
+++ DeepHP_main.py
@@ -16,16 +16,16 @@ import _pickle as pickle
 import Data_Preparation.data_preparation as data_preparation
 import DeepHP.dl_pipeline as dl_pipeline
 import Utils.metrics as metrics
-import Utils.utils as utils
+
 
 if __name__ == "__main__":
-    data_file = './data'
+    data_folder = './data'
     train_list_file = './Data_Preparation/cases_train.txt'
     val_list_file = './Data_Preparation/cases_val.txt'
     test_list_file = './Data_Preparation/cases_test.txt'
     data_split = [train_list_file, val_list_file, test_list_file]
 
-    Dataset_paths = data_preparation.prepare_IDC_Data(data_file, data_split=data_split)
+    Dataset_paths = data_preparation.prepare_IDC_Data(data_folder, data_split=data_split)
     Dataset_np = dl_pipeline.dataset_np(Dataset_paths)
 
     dl_pipeline.train_dl(Dataset_np)
